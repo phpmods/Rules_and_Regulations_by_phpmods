ad1aa374b589bac2ddfbdbdc288e6c754a275ca4
php-MODS <phpmods@users.noreply.github.com>
2015-02-11 11:38:56 +0200

    Update v1.2

--- core/common/RuleregsData.class.php
+++ core/common/RuleregsData.class.php
@@ -1,65 +1,59 @@
 <?php
 ///////////////////////////////////////////////
-///Rules and Regulations v1.1 by php-mods.eu///
+///Rules and Regulations v1.2 by php-mods.eu///
 ///            Author php-mods.eu           ///
-///           Packed at 30/12/2012          ///
-///     Copyright (c) 2013, php-mods.eu     ///
+///            Packed at 11/2/2015          ///
+///     Copyright (c) 2015, php-mods.eu     ///
 ///////////////////////////////////////////////
 
 class RuleregsData extends CodonData {
 	
-        public function getAllRuleCat() {
-        $sql = "SELECT * FROM " . TABLE_PREFIX . "rules_categories WHERE status='1' ORDER BY id";
+	public function getAllRuleCat() {
+		$sql = "SELECT * FROM " . TABLE_PREFIX . "rules_categories WHERE status='1' ORDER BY id";
         return DB::get_results($sql);
-        }
-		public function rulesincat($id) {
+	}
+	public function rulesincat($id) {
 		$sql = "SELECT * FROM " . TABLE_PREFIX . "rules WHERE category = '".$id."' ORDER BY id";
 		return DB::get_results($sql);
-		}
-        public function categories() {
+	}
+	public function categories() {
 		$sql = "SELECT * FROM " . TABLE_PREFIX . "rules_categories ORDER BY id";
 		return DB::get_results($sql);
-		}
-		public function ruleinfo($id)
-		{
+	}
+	public function ruleinfo($id) {
 		$sql = "SELECT * FROM " . TABLE_PREFIX . "rules_categories WHERE id='".$id."' ORDER BY id";
 		return DB::get_row($sql);
-		}
-		public function delete_rule($id)  
-		{
+	}
+	public function delete_rule($id) {
 		$sql = "DELETE FROM " . TABLE_PREFIX . "rules WHERE id='$id'";
 		return DB::get_results($sql);
-	    }
-		public function edit_rule($id)
-		{
+	}
+	public function edit_rule($id) {
 		$sql = "SELECT * FROM " . TABLE_PREFIX . "rules WHERE id='$id'";
 		return DB::get_row($sql);
-		}
-       public function edit_rule_db($rule, $status, $category, $id)
-	    {
-         $query = "UPDATE " . TABLE_PREFIX . "rules SET
-         rule='$rule',
-         status='$status',
-         category='$category'
-         WHERE id='$id'";
-        DB::query($query);
-       }
-	   public function addrule($rule,$status,$category) {
+	}
+	public function edit_rule_db($rule, $status, $category, $id) {
+		$query = "UPDATE " . TABLE_PREFIX . "rules SET
+		rule='$rule',
+		status='$status',
+		category='$category'
+		WHERE id='$id'";
+		DB::query($query);
+	}
+	public function addrule($rule,$status,$category) {
 		$rule = DB::escape($rule);
         $status = DB::escape($status);
         $category = DB::escape($category);
-
-        $sql = "INSERT INTO " . TABLE_PREFIX . "rules
+		$sql = "INSERT INTO " . TABLE_PREFIX . "rules
 					(id, rule, status, category)
 			VALUES	('', '$rule','$status','$category')";
         DB::query($sql);
-	 }
-	 	public function delete_category($id)  
-		{
+	}
+	public function delete_category($id) {
 		$sql = "DELETE FROM " . TABLE_PREFIX . "rules_categories WHERE id='$id'";
 		return DB::get_results($sql);
-	    }
-		public function addcategory($title,$status) {
+	}
+	public function addcategory($title,$status) {
 		$title = DB::escape($title);
         $status = DB::escape($status);
 
@@ -67,13 +61,12 @@ class RuleregsData extends CodonData {
 					(id, title, status)
 			VALUES	('', '$title','$status')";
         DB::query($sql);
-	 }
-	 public function edit_category_db($title, $status, $id)
-	    {
-         $query = "UPDATE " . TABLE_PREFIX . "rules_categories SET
-         title='$title',
-         status='$status'
-         WHERE id='$id'";
-        DB::query($query);
-       }
+	}
+	public function edit_category_db($title, $status, $id) {
+		$query = "UPDATE " . TABLE_PREFIX . "rules_categories SET
+		title='$title',
+		status='$status'
+		WHERE id='$id'";
+		DB::query($query);
+	}
 }
