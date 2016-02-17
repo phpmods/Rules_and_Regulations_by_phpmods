c29588729e4b57acffbd30d17dcf983136296bbe
PHP-Mods <phpmods@users.noreply.github.com>
2016-02-17 23:05:33 +0200

    Update RuleregsData.class.php

--- core/common/RuleregsData.class.php
+++ core/common/RuleregsData.class.php
@@ -1,72 +1,55 @@
 <?php
 ///////////////////////////////////////////////
-///Rules and Regulations v1.2 by php-mods.eu///
+///Rules and Regulations v1.3 by php-mods.eu///
 ///            Author php-mods.eu           ///
-///            Packed at 11/2/2015          ///
-///     Copyright (c) 2015, php-mods.eu     ///
+///            Packed at 17/2/2016          ///
+///     Copyright (c) 2016, php-mods.eu     ///
 ///////////////////////////////////////////////
 
 class RuleregsData extends CodonData {
 	
-	public function getAllRuleCat() {
-		$sql = "SELECT * FROM " . TABLE_PREFIX . "rules_categories WHERE status='1' ORDER BY id";
-        return DB::get_results($sql);
+	static function getAllRuleCat() {
+		$sql = "SELECT * FROM ".TABLE_PREFIX."rules_categories WHERE status=1 ORDER BY id";
+        	return DB::get_results($sql);
 	}
-	public function rulesincat($id) {
-		$sql = "SELECT * FROM " . TABLE_PREFIX . "rules WHERE category = '".$id."' ORDER BY id";
+	static function rulesincat($id) {
+		$sql = "SELECT * FROM ".TABLE_PREFIX."rules WHERE category = '$id' ORDER BY id";
 		return DB::get_results($sql);
 	}
-	public function categories() {
-		$sql = "SELECT * FROM " . TABLE_PREFIX . "rules_categories ORDER BY id";
+	static function categories() {
+		$sql = "SELECT * FROM ".TABLE_PREFIX."rules_categories ORDER BY id";
 		return DB::get_results($sql);
 	}
-	public function ruleinfo($id) {
-		$sql = "SELECT * FROM " . TABLE_PREFIX . "rules_categories WHERE id='".$id."' ORDER BY id";
+	static function ruleinfo($id) {
+		$sql = "SELECT * FROM ".TABLE_PREFIX."rules_categories WHERE id='$id' ORDER BY id";
 		return DB::get_row($sql);
 	}
-	public function delete_rule($id) {
-		$sql = "DELETE FROM " . TABLE_PREFIX . "rules WHERE id='$id'";
+	static function delete_rule($id) {
+		$sql = "DELETE FROM ".TABLE_PREFIX."rules WHERE id='$id'";
 		return DB::get_results($sql);
 	}
-	public function edit_rule($id) {
-		$sql = "SELECT * FROM " . TABLE_PREFIX . "rules WHERE id='$id'";
+	static function edit_rule($id) {
+		$sql = "SELECT * FROM ".TABLE_PREFIX."rules WHERE id='$id'";
 		return DB::get_row($sql);
 	}
-	public function edit_rule_db($rule, $status, $category, $id) {
-		$query = "UPDATE " . TABLE_PREFIX . "rules SET
-		rule='$rule',
-		status='$status',
-		category='$category'
-		WHERE id='$id'";
-		DB::query($query);
+	static function edit_rule_db($rule, $status, $category, $id) {
+		$sql = "UPDATE ".TABLE_PREFIX."rules SET rule='$rule', status='$status', category='$category' WHERE id='$id'";
+		DB::query($sql);
 	}
-	public function addrule($rule,$status,$category) {
-		$rule = DB::escape($rule);
-        $status = DB::escape($status);
-        $category = DB::escape($category);
-		$sql = "INSERT INTO " . TABLE_PREFIX . "rules
-					(id, rule, status, category)
-			VALUES	('', '$rule','$status','$category')";
-        DB::query($sql);
+	static function addrule($rule,$status,$category) {
+		$sql = "INSERT INTO ".TABLE_PREFIX."rules (id, rule, status, category) VALUES ('', '$rule','$status','$category')";
+        	DB::query($sql);
 	}
-	public function delete_category($id) {
-		$sql = "DELETE FROM " . TABLE_PREFIX . "rules_categories WHERE id='$id'";
+	static function delete_category($id) {
+		$sql = "DELETE FROM ".TABLE_PREFIX."rules_categories WHERE id='$id'";
 		return DB::get_results($sql);
 	}
-	public function addcategory($title,$status) {
-		$title = DB::escape($title);
-        $status = DB::escape($status);
-
-        $sql = "INSERT INTO " . TABLE_PREFIX . "rules_categories
-					(id, title, status)
-			VALUES	('', '$title','$status')";
-        DB::query($sql);
+	static function addcategory($title,$status) {
+        	$sql = "INSERT INTO ".TABLE_PREFIX."rules_categories (id, title, status) VALUES ('', '$title','$status')";
+        	DB::query($sql);
 	}
-	public function edit_category_db($title, $status, $id) {
-		$query = "UPDATE " . TABLE_PREFIX . "rules_categories SET
-		title='$title',
-		status='$status'
-		WHERE id='$id'";
-		DB::query($query);
+	static function edit_category_db($title, $status, $id) {
+		$sql = "UPDATE ".TABLE_PREFIX."rules_categories SET title='$title', status='$status' WHERE id='$id'";
+		DB::query($sql);
 	}
 }
